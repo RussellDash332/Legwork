c31a81893e82c60c7a69ee201a272d560698762c
xBoommy <boommyboy99@gmail.com>
2022-10-19 23:11:25 +0800

    Update firebase configuration

--- src/frontend/project-app/src/api/firebase-config.js
+++ src/frontend/project-app/src/api/firebase-config.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCJW1lRKOSw1zbfyQ3wjG8tB2pOWgLkRzA",
-    authDomain: "workout-f1285.firebaseapp.com",
-    projectId: "workout-f1285",
-    storageBucket: "workout-f1285.appspot.com",
-    messagingSenderId: "680892455956",
-    appId: "1:680892455956:web:1ec13e708310b1e46c1b01"
-};
+    apiKey: "AIzaSyBhXtKtJy5DoqQOq6U6xE0Hy5tEV3SKxvo",
+    authDomain: "dsa3101-project.firebaseapp.com",
+    projectId: "dsa3101-project",
+    storageBucket: "dsa3101-project.appspot.com",
+    messagingSenderId: "7578463665",
+    appId: "1:7578463665:web:f0a6e7e95ab436c7c4a190"
+  };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
