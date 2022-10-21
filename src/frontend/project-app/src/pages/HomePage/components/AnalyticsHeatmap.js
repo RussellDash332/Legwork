4bee84b30d2e8b141b6cc2dc2608a563e501efca
Megan Ng <e0559708@u.nus.edu>
2022-10-21 18:55:57 +0800

    Init leaflet map

--- src/frontend/project-app/src/pages/HomePage/components/AnalyticsHeatmap.js
+++ src/frontend/project-app/src/pages/HomePage/components/AnalyticsHeatmap.js
@@ -1,10 +1,24 @@
 import React from "react";
+import { MapContainer, ImageOverlay } from "react-leaflet"
+import "leaflet/dist/leaflet.css";
 
 const AnalyticsHeatmap = () => {
+    const bounds = [[600, 0],[0, 300]];
+    const center = [65, 150];
+
     return (
-        <div className="card w-96 h-2/5 shadow-xl bg-orange-400">
-            AnalyticsHeatmap
-        </div>
+        <MapContainer 
+            center={center} 
+            zoom={2} 
+            scrollWheelZoom={false}
+            style={{ height: '100vh', width: '100%' }}
+        >
+            <ImageOverlay 
+                attribution="&copy; Developed by LegWork Inc." 
+                url={require("../../../assets/images/Test.png")} 
+                bounds={bounds} 
+            />
+        </MapContainer>
     );
 }
 
