dbdb4839bd8fb8716092bd3eb7313928667e81ed
geraldw12 <geraldwanghw@gmail.com>
2022-10-27 15:36:50 +0800

    changed size of heatmap,linechart and input data

--- src/frontend/project-app/src/pages/HomePage/components/AnalyticsHeatmap.js
+++ src/frontend/project-app/src/pages/HomePage/components/AnalyticsHeatmap.js
@@ -1,9 +1,14 @@
 import React from "react";
 
+// import Heatmap from "./Heatmap";
+
 const AnalyticsHeatmap = () => {
     return (
-        <div className="card w-11/12 h-2/5 shadow-xl bg-sky-600">
-            AnalyticsHeatmap
+        <div className="card w-11/12 h-45 shadow-xl bg-sky-600">
+            <div>
+                {/* <Heatmap /> */}
+                Heatmap
+            </div>
         </div>
     );
 }
