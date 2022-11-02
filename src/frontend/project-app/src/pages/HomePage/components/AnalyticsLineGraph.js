d3b1be2a0ad8c9d6a2207bc198de4c70504d605c
geraldw12 <geraldwanghw@gmail.com>
2022-11-02 16:43:56 +0800

    removed extra linegraph2

--- src/frontend/project-app/src/pages/HomePage/components/AnalyticsLineGraph.js
+++ src/frontend/project-app/src/pages/HomePage/components/AnalyticsLineGraph.js
@@ -1,15 +1,15 @@
 import React from "react";
 import Plot from 'react-plotly.js';
 import LineGraph from "./LineGraph"
-import LineGraph2 from "./LineGraph2";
+
 
 const AnalyticsLineGraph = () => {
     return (
         <div className="card w-10/12 h-full shadow-xl bg-base-200">
             
             <div>
-                {/* <LineGraph /> */}
-                <LineGraph2 />
+                <LineGraph />
+                
                 
             </div>
         </div>
