071f01bb6c0d5d354ca8ea28201ceef845661f89
xBoommy <boommyboy99@gmail.com>
2022-11-07 14:09:28 +0800

    Add build configs

--- src/frontend/project-app/public/main.js
+++ src/frontend/project-app/public/main.js
@@ -1,4 +1,6 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, protocol } = require("electron");
+const path = require("path");
+const url = require("url");
 
 function createWindow () {
   // Create the browser window.
@@ -8,15 +10,27 @@ function createWindow () {
     minWidth:800,
     minHeight:600,
     webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
       nodeIntegration: true
     }
   })
 
-  // Load the index.html from a url
-  win.loadURL('http://localhost:3000');
+  // In production, set the initial browser path to the local bundle generated
+  // by the Create React App build process.
+  // In development, set it to localhost to allow live/hot-reloading.
+  const appURL = app.isPackaged
+    ? url.format({
+        pathname: path.join(__dirname, "index.html"),
+        protocol: "file:",
+        slashes: true,
+      })
+    : "http://localhost:3000";
+  win.loadURL(appURL);
 
-  // Open the DevTools.
-  win.webContents.openDevTools()
+  // Automatically open Chrome's DevTools in development mode.
+  if (!app.isPackaged) {
+    win.webContents.openDevTools();
+  }
 
   // Remove top option menus
   win.removeMenu();
@@ -25,10 +39,36 @@ function createWindow () {
   win.maximize();
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
+// Setup a local proxy to adjust the paths of requested files when loading
+// them from the local production bundle (e.g.: local fonts, etc...).
+function setupLocalFilesNormalizerProxy() {
+  protocol.registerHttpProtocol(
+    "file",
+    (request, callback) => {
+      const url = request.url.substr(8);
+      callback({ path: path.normalize(`${__dirname}/${url}`) });
+    },
+    (error) => {
+      if (error) console.error("Failed to register protocol");
+    }
+  );
+}
+
+// This method will be called when Electron has finished its initialization and
+// is ready to create the browser windows.
 // Some APIs can only be used after this event occurs.
-app.whenReady().then(createWindow)
+app.whenReady().then(() => {
+  createWindow();
+  setupLocalFilesNormalizerProxy();
+
+  app.on("activate", function () {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
